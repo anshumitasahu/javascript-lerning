624d63f958b209197c6a1f1e6f63558c43fba022
Anshumita Sahu <anshumitasahu.official@gmail.com>
2026-04-22 21:13:54 +0530

    learning

---
+++ dom_basics.js
@@ -0,0 +1,19 @@
+// let choices = ["Snake", "Water", "Gun"];
+// let choice = prompt("Enter the desired choice: 'Snake', 'Water', 'Gun'");
+// let compChoose = choices[Math.floor(Math.random() * choices.length)];
+// if (choice == compChoose) {
+//     console.log("draw")
+// } else if (choice == "Snake" && compChoose == "Water") {
+//     console.log("You win")
+// } else if (choice == "Snake" && compChoose == "Gun") {
+//     console.log("computer wins")
+// } else if (choice == "Gun" && compChoose == "Snake") {
+//     console.log("You win")
+// } else if (choice == "Gun" && compChoose == "Water") {
+//     console.log("computer wins")
+// } else if (choice == "Water" && compChoose == "Snake") {
+//     console.log("computer wins")
+// } else if (choice == "Water" && compChoose == "Gun") {
+//     console.log("You win")
+// }
+
