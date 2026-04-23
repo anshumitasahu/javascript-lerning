8c031079ce08382f1cbbded4d41314b258ccf165
Anshumita Sahu <anshumitasahu.official@gmail.com>
2026-04-23 19:51:06 +0530

    learnig again

--- dom.js
+++ dom.js
@@ -50,4 +50,51 @@
 //             `;
 //         }
 //     }1
+// // }
+// let first = document.getElementById("first");
+// let a = first.getAttribute("class");
+// console.log(a);
+// console.log(first.hasAttribute("class"));
+// console.log(first.hasAttribute("style"));
+// // first.setAttribute("hidden", "true sachin");
+// // first.removeAttribute("class");
+// console.log(first.attributes);
+
+// let a = document.getElementsByClassName("elem")[0]
+// a.innerHTML = a.innerHTML + `<h1>Hello</h1>`;
+
+// let div= document.createElement(`div`);
+// div.innerHTML = "hello there";
+// a.appendChild(div);
+// console.log (a.classList);
+// a.classList.remove("kelem");
+// console.log (a.classList);
+// a.classList.add("kelem");
+// alert("hello");
+// let t = setTimeout(function () { alert("mememmeememmemmememememe")}, 5000);
+// let s = prompt("do you want the reply")
+// if ("n" == s) {
+//    clearTimeout(t); 
 // }
+// console.log(t);
+// const result = (x, y, z) => {
+    // let sum = x + y + z;
+    // console.log(sum);
+// }
+// setTimeout(result, 2000, 1, 2, 4);
+// setInterval(result, 2000, 1, 2, 4);
+// document.getElementById("google").addEventListener("click", function() {
+//     let url = "https://www.google.com/search?q=javascript";
+//     let win = window.open(url, "", "width=500,height=500,scrollbars=yes,resize=yes")
+// })
+// document.getElementById("x").addEventListener("click", function() {
+//     let url = "https://x.com/home";
+//     let win = window.open(url, "", "width=500,height=500,scrollbars=yes,resize=yes")
+// })
+let a = document.querySelector("#bulb")
+setInterval(async function() {
+    document.querySelector("#bulb").classList.toggle("bulb")
+}, 500)
+console.log(a.classList)
+a.classList.remove("bulb")
+console.log(a.classList)
--- dom_basics.js
+++ dom_basics.js
@@ -2,18 +2,19 @@
 // let choice = prompt("Enter the desired choice: 'Snake', 'Water', 'Gun'");
 // let compChoose = choices[Math.floor(Math.random() * choices.length)];
 // if (choice == compChoose) {
-//     console.log("draw")
+//     console.alert("draw")
 // } else if (choice == "Snake" && compChoose == "Water") {
-//     console.log("You win")
+//     console.alert("You win")
 // } else if (choice == "Snake" && compChoose == "Gun") {
-//     console.log("computer wins")
+//     console.alert("computer wins")
 // } else if (choice == "Gun" && compChoose == "Snake") {
-//     console.log("You win")
+//     console.alert("You win")
 // } else if (choice == "Gun" && compChoose == "Water") {
-//     console.log("computer wins")
+//     console.alert("computer wins")
 // } else if (choice == "Water" && compChoose == "Snake") {
-//     console.log("computer wins")
+//     console.alert("computer wins")
 // } else if (choice == "Water" && compChoose == "Gun") {
-//     console.log("You win")
+//     console.alert("You win")
 // }
 
+// practice set 
